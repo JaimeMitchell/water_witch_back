39194ffc95fc705bd28bda5ab4a86141d527bb90
Jaime Mitchell <JaimeMitchell100@gmail.com>
2023-01-29 09:40:07 -0500

    needed to start over and toss my github repo, so new remote, old repo

---
+++ waterwitchBackup.sql
@@ -0,0 +1,123 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.6 (Homebrew)
+-- Dumped by pg_dump version 14.6 (Homebrew)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: alembic_version; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.alembic_version (
+    version_num character varying(32) NOT NULL
+);
+
+
+ALTER TABLE public.alembic_version OWNER TO postgres;
+
+--
+-- Name: fountains; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.fountains (
+    id integer NOT NULL,
+    latitude double precision NOT NULL,
+    longitude double precision NOT NULL,
+    name character varying,
+    details character varying,
+    borough character varying
+);
+
+
+ALTER TABLE public.fountains OWNER TO postgres;
+
+--
+-- Name: fountains_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.fountains_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.fountains_id_seq OWNER TO postgres;
+
+--
+-- Name: fountains_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.fountains_id_seq OWNED BY public.fountains.id;
+
+
+--
+-- Name: fountains id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.fountains ALTER COLUMN id SET DEFAULT nextval('public.fountains_id_seq'::regclass);
+
+
+--
+-- Data for Name: alembic_version; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.alembic_version (version_num) FROM stdin;
+5d08493ff01c
+\.
+
+
+--
+-- Data for Name: fountains; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.fountains (id, latitude, longitude, name, details, borough) FROM stdin;
+2	-73.98649181365882	40.60753207315601	Seth Low Playground/ Bealin Square	High Low, Out in Open	B
+\.
+
+
+--
+-- Name: fountains_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.fountains_id_seq', 2, true);
+
+
+--
+-- Name: alembic_version alembic_version_pkc; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.alembic_version
+    ADD CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num);
+
+
+--
+-- Name: fountains fountains_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.fountains
+    ADD CONSTRAINT fountains_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
