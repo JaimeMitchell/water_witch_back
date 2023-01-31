6ad6cad1ef43ee4737f80059713f58776e0612a6
Jaime Mitchell <JaimeMitchell100@gmail.com>
2023-01-31 16:23:15 -0500

    added script and changed model back to lat and lon

--- app/models/fountain.py
+++ app/models/fountain.py
@@ -1,19 +1,52 @@
 from app import db
 from sqlalchemy import *
-from geoalchemy2 import *
+from geoalchemy2 import Geometry
+# from geojson import *
 # from geoalchemy2 import Geometry, WKTElement
 # from geoalchemy2 import Geometry, WKTElement,  ST_Intersects
 # from sqlalchemy.dialects.postgresql import ST_AsGeoJSON, ST_Intersects
-
+# from app import db
 
 class Fountain(db.Model):
     __tablename__ = "fountains"
 
     id = db.Column(db.Integer, primary_key=True)
-    geometry = db.Column(Geometry(geometry_type='POINT', srid=4326))
-    name = db.Column(db.String, nullable=True)
-    details = db.Column(db.String, nullable=True)
-    borough = db.Column(db.String, nullable=True)
+    latitude= db.Column(db.Float, nullable=False)
+    longitude=db.Column(db.Float, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    details = db.Column(db.String, nullable=False)
+    borough = db.Column(db.String, nullable=False)
+
+    
+        
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'latitude':self.latitude,
+            'longitude':self.longitude,
+            'name': self.name,
+            'details': self.details,
+            'borough': self.borough
+        }
+
+    @classmethod
+    def to_object(cls, data_dict):
+        return cls(
+            latitude=data_dict["latitude"],
+            longitude=data_dict["longitude"],
+            name=data_dict["name"],
+            details=data_dict["details"],
+            borough=data_dict["borough"])
+
+
+# class Fountain(db.Model):
+#     __tablename__ = "fountains"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     geometry = db.Column(Geometry(geometry_type='POINT', srid=4326))
+#     name = db.Column(db.String, nullable=True)
+#     details = db.Column(db.String, nullable=True)
+#     borough = db.Column(db.String, nullable=True)
 
     # def to_dict(self):
     #     return {
@@ -24,23 +57,23 @@ class Fountain(db.Model):
     #         'borough': self.borough
     #     }
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'geometry': self.geometry.wkt,
-            'name': self.name,
-            'details': self.details,
-            'borough': self.borough,
+    # def to_dict(self):
+    #     return {
+    #         'id': self.id,
+    #         'geometry': self.geometry.wkt,
+    #         'name': self.name,
+    #         'details': self.details,
+    #         'borough': self.borough,
 
-        }
+    #     }
 
-    @classmethod
-    def to_object(cls, data_dict):
-        return cls(
-            geometry=data_dict["geometry"],
-            name=data_dict["name"],
-            details=data_dict["details"],
-            borough=data_dict["borough"])
+    # @classmethod
+    # def to_object(cls, data_dict):
+    #     return cls(
+    #         geometry=data_dict["geometry"],
+    #         name=data_dict["name"],
+    #         details=data_dict["details"],
+    #         borough=data_dict["borough"])
 
     # @classmethod
     # def get_clustered_markers(cls, bbox):
@@ -72,12 +105,14 @@ class Fountain(db.Model):
 #     __tablename__ = "fountains"
 
 #     id = db.Column(db.Integer, primary_key=True)
-#     latitude= db.Column(db.Float(row[lat]), nullable=False)
-#     longitude=db.Column(db.Float(row[lon]), nullable=False)
-#     name = db.Column(db.String, nullable=True)
-#     details = db.Column(db.String, nullable=True)
-#     borough = db.Column(db.String, nullable=True)
+#     latitude= db.Column(db.Float, nullable=False)
+#     longitude=db.Column(db.Float, nullable=False)
+#     name = db.Column(db.String, nullable=False)
+#     details = db.Column(db.String, nullable=False)
+#     borough = db.Column(db.String, nullable=False)
 
+    
+        
 #     def to_dict(self):
 #         return {
 #             'id': self.id,
---
+++ seed.py
@@ -0,0 +1,108 @@
+# from app import db
+
+# from app import create_app, db
+# from app.models.fountain import Fountain
+
+
+# fountains = [
+#     {
+#         "type": "FeatureCollection",
+#         "features": [
+#             {"type": "Feature",
+#             "properties": { "name": "Pink Playground",
+#             "details": "In Playground",
+#             "borough": "B",
+#             "latitude": 40.666701390972364,
+#             "longitude": -73.862594500714295 }
+#             }
+#         ]
+#     }
+# ]
+
+# THIS IS JUST A REFERENCE TO LOOK AT, IT LIVES IN UR MODEL
+# class Fountain(db.Model):
+#     __tablename__ = "fountains"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     latitude= db.Column(db.Float, nullable=False)
+#     longitude=db.Column(db.Float, nullable=False)
+#     name = db.Column(db.String, nullable=False)
+#     details = db.Column(db.String, nullable=False)
+#     borough = db.Column(db.String, nullable=False)
+
+
+# def getFountains():
+#     list_of_fountains_to_add_to_db = []
+#     for fountain in fountains:
+#         fountain_to_add = Fountain(
+#             latitude=fountain.features[0].property.latitude,
+#             longitude=fountain.features[0].property.longitude,
+#             name="Name",
+#             details="Details",
+#             borough="Borough"
+#         )
+#         list_of_fountains_to_add_to_db.append(fountain_to_add)
+#     return list_of_fountains_to_add_to_db
+
+
+from app import create_app, db
+from app.models.fountain import Fountain
+
+fountains = [
+    {
+        "latitude": 40.666701390972364,
+        "longitude": -73.862594500714295,
+        "name": "Pink Playground",
+        "details": "In Playground",
+        "borough": "B"
+    },
+    {
+        "latitude": 40.6756201057255,
+        "longitude": -73.863013059621238,
+        "name": "Belmont Playground",
+        "details": "In Playground",
+        "borough": "B"
+    },
+
+    {
+        "latitude": 40.674530476243746,
+        "longitude": -73.864882054307543,
+        "name": "Robert E. Venable Park",
+        "details": "Just Outside Playground",
+        "borough": "B"
+
+    }
+
+]
+
+
+def getFountains():
+    list_of_fountains_to_add_to_db = []
+    for fountain in fountains:
+        fountain_to_add = Fountain(
+            latitude=fountain.latitude,
+            longitude=fountain.longitude,
+            name=fountain.name,
+            details=fountain.details,
+            borough=fountain.borough
+        )
+        list_of_fountains_to_add_to_db.append(fountain_to_add)
+    return list_of_fountains_to_add_to_db
+
+
+def load():
+    app = create_app()
+
+    fountains = getFountains()
+
+    with app.app_context():
+        db.session.add_all(fountains)
+        db.session.commit()
+
+
+def main():
+    load()
+
+
+if __name__ == "__main__":
+    main()
